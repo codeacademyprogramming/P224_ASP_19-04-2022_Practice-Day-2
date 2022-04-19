a93588228e4a208627c6b79c90a662f3113375a1
HamidMammadov <hamidvm@code.edu.az>
2022-04-19 19:14:28 +0400

    First Commit

---
+++ P224Allup/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Models
+{
+    public class Category : BaseEntity
+    {
+        [StringLength(255),Required]
+        public string Name { get; set; }
+        [StringLength(1000)]
+        public string Image { get; set; }
+        public Nullable<int> ParentId { get; set; }
+        public Category Parent { get; set; }
+        public IEnumerable<Category> Children { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
